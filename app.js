944de0dec9ed8531a08b92c2c8f6f3cc566055b8
c-costin <contact@costincadeau.fr>
2023-08-27 12:44:52 +0200

    feat: rewrite url and change creator name for link

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ import Turbo from "turbolinks";
 
 const App = {
     init: () => {
+        App.replaceURL();
         Turbo.start();
         Turbo.setProgressBarDelay(100);
     },
+    replaceURL: () => {
+        window.history.replaceState(null, null, window.location.pathname.split('.html')[0]);
+    }
 };
 
 document.addEventListener("DOMContentLoaded", App.init);
