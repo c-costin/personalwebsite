d4c2515946af6b453eeaf8c15992d4d7a56af4d7
c-costin <contact@costincadeau.fr>
2023-11-20 19:19:04 +0100

    build(app): change apdater for static

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,12 +1,18 @@
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: true,
+			strict: true
+		})
 	}
 };
 
