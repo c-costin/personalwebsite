cdde99a8340550deeb6bdd312edb90868954c659
c-costin <contact@costincadeau.fr>
2023-11-24 20:32:24 +0100

    update(deploy): adding tailingSlash option

--- app/src/routes/+layout.server.ts
+++ app/src/routes/+layout.server.ts
@@ -1,6 +1,7 @@
 import type { LayoutServerLoad } from './$types';
 
 export const ssr = false;
+export const trailingSlash = 'always';
 
 export const load: LayoutServerLoad = async ({ locals, url }) => {
 	return {
